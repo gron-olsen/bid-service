6a99bd3df32f98b1c0c4da2cba3513e70d295db7
casper hansen <casperhansen123@hotmail.com>
2024-05-30 09:20:27 +0200

    1.0.0 Release

--- bid-service/Models/bid.cs
+++ bid-service/Models/bid.cs
@@ -4,7 +4,7 @@ public class Bid
 {
 
     public int BidID { get; set; }   
-    public int AuctionID {get; set;}
+    public int ProductID {get; set;}
     
     public int BidUserID { get; set; }
     public int BidPrice {get; set;}
